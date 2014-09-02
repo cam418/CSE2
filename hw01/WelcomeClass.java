2ef93619b2630672e18e2d7eedb2ee2aa133670f
cam418 <cam418@lehigh.edu>
2014-09-02 23:59:44 +0000

    HW01 Update

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////
+//Charles Mandala
+//hw01
+//WelcomeClass Java Program
+//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement with box around it
+    System.out.println("  -----------");
+    System.out.println(" | WELCOME |");
+    System.out.println(" -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+//    System.out.println(" / \/ \ / \ / \ / \ / \ ");
+    System.out.println("|-C--A--M--4--1--8-|");
+//    System.out.println("\ /\ /\ /\ /\ /\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("");
+    System.out.println("I moved into Lehigh University Richards dorm on August 21,2014");
+    
+    //test/testtest
+  }
+}
